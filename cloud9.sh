aad9f2a746fd71fc864b2148f309754c587a8515
franklinaws <aguinalf@amazon.com>
2022-08-25 22:44:28 +0000

    adding script

---
+++ cloud9.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# sudo curl --silent --location -o /usr/local/bin/kubectl \
+#   https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
+
+# sudo chmod +x /usr/local/bin/kubectl
+
+# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+# unzip awscliv2.zip
+# sudo ./aws/install
+
+# sudo yum -y install jq gettext bash-completion moreutils
+
+# for command in kubectl jq envsubst aws
+#   do
+#     which $command &>/dev/null && echo "$command in path" || echo "$command NOT FOUND"
+#   done
+
+# kubectl completion bash >>  ~/.bash_completion
+# . /etc/profile.d/bash_completion.sh
+# . ~/.bash_completion
+
+# aws cloud9 update-environment  --environment-id $C9_PID --managed-credentials-action DISABLE
+# rm -vf ${HOME}/.aws/credentials
+
+# curl -sS https://webinstall.dev/k9s | bash
+
+# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+# ~/.fzf/install --all
+# sudo curl https://raw.githubusercontent.com/blendle/kns/master/bin/kns -o /usr/local/bin/kns && sudo chmod +x $_
+# sudo curl https://raw.githubusercontent.com/blendle/kns/master/bin/ktx -o /usr/local/bin/ktx && sudo chmod +x $_
+# echo "alias kgn='kubectl get nodes -L beta.kubernetes.io/arch -L eks.amazonaws.com/capacityType -L beta.kubernetes.io/instance-type -L eks.amazonaws.com/nodegroup -L topology.kubernetes.io/zone -L karpenter.sh/provisioner-name -L karpenter.sh/capacity-type'" | tee -a ~/.bashrc
+# echo "alias k=kubectl" | tee -a ~/.bashrc
+# echo "alias kgp='kubectl get pods'" | tee -a ~/.bashrc
+# source ~/.bashrc
+
+# export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
+# export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
+
+# aws sts get-caller-identity --query Arn | grep eks-blueprints-for-terraform-workshop-admin -q && echo "IAM role valid" || echo "IAM role NOT valid"
+
+# git config --global user.name "franklinaws"
+# git config --global user.email aguinalf@amazon.com
+
+# git config --global credential.helper 'cache --timeout=3600'
+
